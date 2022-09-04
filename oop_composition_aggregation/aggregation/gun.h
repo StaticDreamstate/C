d7a89977e7a849a90e845933c145617f25a998fb
StaticDreamstate <lonely_loonie@protonmail.com>
2022-09-04 03:40:06 -0300

    New files

---
+++ oop_composition_aggregation/aggregation/gun.h
@@ -0,0 +1,16 @@
+#ifndef GUN_H
+#define GUN_H
+typedef int bool_t;
+// Type forward declarations
+struct gun_t;
+// Memory allocator
+struct gun_t* gun_new();
+// Constructor
+void gun_ctor(struct gun_t*, int);
+// Destructor
+void gun_dtor(struct gun_t*);
+// Behavior functions
+bool_t gun_has_bullets(struct gun_t*);
+void gun_trigger(struct gun_t*);
+void gun_refill(struct gun_t*);
+#endif
